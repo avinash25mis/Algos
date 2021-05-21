7ee1346205a7bbdd237dcd5e4468d0028c13ae8f
Avinash Mishra <avinash.a.mishra@ericsson.com>
2021-05-21 18:35:56 +0530

    Linklist

---
+++ problems/src/com/linkedList/Solution.java
@@ -0,0 +1,45 @@
+package com.linkedList;
+
+/**
+ * @author avinash.a.mishra
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+
+    public static void main(String[] args) {
+        addTwoNumbers(null,null);
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        ListNode result= new ListNode();
+
+        ListNode  t1=l1;
+        ListNode  t2=l2;
+        ListNode re=result;
+
+        do {
+            re.val= t1.val+t2.val;
+            t1=t1.next;
+            t2=t2.next;
+            if(t1!=null && t2!=null) {
+                ListNode newNode= new ListNode();
+                re.next=newNode;
+                re=re.next;
+            }
+
+        }while(t1!=null && t2!=null);
+
+        return result;
+    }
+}
